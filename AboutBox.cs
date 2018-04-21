bf32529fe4eaf841642b77ee01eac8e18537423d
mingzhang6 <38576952+mingzhang6@users.noreply.github.com>
2018-04-21 12:22:17 +0800

    Add files via upload

---
+++ AboutBox.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSVtoDatabase
+{
+    partial class AboutBox : Form
+    {
+        private static AboutBox aboutBox = new AboutBox();
+        public AboutBox()
+        {
+            InitializeComponent();
+            this.Text = String.Format("About {0}", AssemblyTitle);
+            this.labelProductName.Text = AssemblyProduct;
+            this.labelVersion.Text = String.Format("Version {0}", AssemblyVersion);
+            this.labelCopyright.Text = AssemblyCopyright;
+            this.labelCompanyName.Text = AssemblyCompany;
+            this.textBoxDescription.Text = AssemblyDescription;
+
+        }
+
+        public new static void Show()
+        {
+            ((Control) aboutBox).Show();
+             //AboutBox.Show();
+        }
+
+        public new static void ShowDialog()
+        {
+            ((Control) aboutBox).Show();
+            //AboutBox.ShowDialog();
+        }
+        #region Assembly Attribute Accessors
+
+        public string AssemblyTitle
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyTitleAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    AssemblyTitleAttribute titleAttribute = (AssemblyTitleAttribute)attributes[0];
+                    if (titleAttribute.Title != "")
+                    {
+                        return titleAttribute.Title;
+                    }
+                }
+                return System.IO.Path.GetFileNameWithoutExtension(Assembly.GetExecutingAssembly().CodeBase);
+            }
+        }
+
+        public string AssemblyVersion
+        {
+            get
+            {
+                return "1.0.0"; //Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            }
+        }
+
+        public string AssemblyDescription
+        {
+            get
+            {
+                string description = @"This software is developed by Johnny Luo (Luo Mingzhang). ";
+                return description;
+            }
+        }
+
+        public string AssemblyProduct
+        {
+            get
+            {
+                object[] attributes = Assembly.GetExecutingAssembly().GetCustomAttributes(typeof(AssemblyProductAttribute), false);
+                if (attributes.Length == 0)
+                {
+                    return "";
+                }
+                return ((AssemblyProductAttribute)attributes[0]).Product;
+            }
+        }
+
+        public string AssemblyCopyright
+        {
+            get
+            {
+                return string.Empty;// "Octopos Co., Ltd";
+            }
+        }
+
+        public string AssemblyCompany
+        {
+            get
+            {
+                return string.Empty; //"Octopos Co., Ltd";
+            }
+        }
+        #endregion
+
+        private void okButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void textBoxDescription_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
